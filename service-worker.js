771373bfb6c7e5c5fa3d05624d30e9d35b8edb65
Pedro Henrique Mantovani <phmantovani001@edu.unifil.br>
2026-09-23 10:54:37 -0300

    Adicionado mode de instalaçao

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'flos-pijamas-v1';
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/admin/',
+  '/admin/index.html',
+  '/admin/admin.css',
+  '/admin/admin.js',
+  '/manifest.webmanifest',
+  '/img/logo.png',
+  '/img/icon-192.png',
+  '/img/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (response.ok) {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+        }
+        return response;
+      });
+    })
+  );
+});
